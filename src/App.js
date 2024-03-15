012cb2c0c5a3b7e394bf95f403f77dce9c26b826
abid982 <abidarif982@gmail.com>
2024-03-15 11:29:32 +0500

    Create a state variable to update step with usestate

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const messages = [
   'Learn React ⚛️',
   'Apply for jobs 💼',
@@ -5,35 +7,51 @@ const messages = [
 ];
 
 function App() {
-  const step = 2;
+  // const step = 2;
+  // const arr = useState(1);
+  // console.log(arr);
+  const [step, setStep] = useState(1);
+  // const [test, setTest] = useState({ name: 'Jonas' });
 
   function handlePrevious() {
-    alert('Previous step');
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  }
+
+  function handleNext() {
+    if (step < 3) {
+      setStep(step + 1);
+    }
+    // BAD PRACTICE
+    // step = step + 1;
+    // test.name = 'John';
+    // setTest({ name: 'John' });
   }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+        <div className={step >= 1 ? 'active' : ''}>1</div>
+        <div className={step >= 2 ? 'active' : ''}>2</div>
+        <div className={step >= 3 ? 'active' : ''}>3</div>
       </div>
 
       <p className="message">
         Step {step}: {messages[step - 1]}
+        {/* {test.name} */}
       </p>
 
       <div className="buttons">
         <button
           style={{ backgroundColor: '#7950f2', color: '#fff' }}
-          onClick={() => alert('Previous button')}
+          onClick={handlePrevious}
         >
           Previous
         </button>
         <button
           style={{ backgroundColor: '#7950f2', color: '#fff' }}
-          onClick={handlePrevious}
-          onMouseEnter={() => alert('TEST')}
+          onClick={handleNext}
         >
           Next
         </button>
